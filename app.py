0d6e434ed6e0e8b8f576bb5f5af57140050fc22f
Tushar Rohilla <tusharrohilla70@gmail.com>
2025-10-12 21:35:08 +0530

    valon

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Duniya valon! kaise ho'
+    return 'Hello, Duniya valon! kaise ho aap sb'
 
 if __name__ == '__main__':
     app.run()
