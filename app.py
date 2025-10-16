c0af89ce739b5978d473e822e5809b64bf677624
Tushar Rohilla <tusharrohilla70@gmail.com>
2025-10-16 09:20:26 +0530

    Theek ho kya

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Duniya valon! kaise ho aap sb'
+    return 'Hello, Duniya valon! kaise ho aap sb, theek ho kya ?'
 
 if __name__ == '__main__':
     app.run()
